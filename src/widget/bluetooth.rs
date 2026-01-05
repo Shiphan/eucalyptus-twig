96377dd76964a2f0aa74e2d154cd16e8fc85c980
Shiphan <140245703+Shiphan@users.noreply.github.com>
2026-01-05 16:31:12 +0800

    Improve error handling in bluetooth widget, simplify connected devices variable and handle cases where the device is already connected to adapter

--- src/widget/bluetooth.rs
+++ src/widget/bluetooth.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 use bluer::{
     Adapter, AdapterEvent, AdapterProperty, Address, DeviceEvent, DeviceProperty, Session,
@@ -13,8 +13,7 @@ pub struct Bluetooth {
     error_message: Option<String>,
     powered: Option<bool>,
     discovering: Option<bool>,
-    devices_connected: HashMap<Address, bool>,
-    connected_count: usize,
+    connected_devices: HashSet<Address>,
 }
 
 impl Widget for Bluetooth {
@@ -25,8 +24,7 @@ impl Widget for Bluetooth {
             error_message: None,
             powered: None,
             discovering: None,
-            devices_connected: HashMap::new(),
-            connected_count: 0,
+            connected_devices: HashSet::new(),
         }
     }
 }
@@ -40,7 +38,7 @@ impl Render for Bluetooth {
                 Some(true) => {
                     if self.discovering == Some(true) {
                         widget_wrapper().child("")
-                    } else if self.connected_count == 0 {
+                    } else if self.connected_devices.len() == 0 {
                         widget_wrapper().child("")
                     } else {
                         widget_wrapper().child("")
@@ -57,163 +55,164 @@ async fn task(this: WeakEntity<Bluetooth>, cx: &mut AsyncApp) {
     let handle = match cx.update(|cx| Tokio::handle(cx)) {
         Ok(x) => x,
         Err(e) => {
+            tracing::error!(error = %e, "Failed to get tokio handle, which is required for bluer crate to work");
             let _ = this.update(cx, |this, cx| {
-                this.error_message = Some(format!("Error while getting tokio handle: {e}"));
+                this.error_message = Some(format!("Failed to get tokio handle: {e}"));
                 cx.notify()
             });
-
-            println!("Error while getting tokio handle: {e}");
             return;
         }
     };
     let _guard = handle.enter();
 
-    match default_adapter().await {
-        Ok(adapter) => {
-            match adapter.is_powered().await {
-                Ok(is_powered) => {
-                    let _ = this.update(cx, |this, cx| {
-                        this.powered = Some(is_powered);
-                        cx.notify()
-                    });
-                }
-                Err(e) => {
-                    let _ = this.update(cx, |this, cx| {
-                        this.error_message = Some(format!("{} error: {e}", line!()));
-                        cx.notify()
-                    });
-                }
-            }
-            match adapter.is_discovering().await {
-                Ok(discovering) => {
-                    let _ = this.update(cx, |this, cx| {
-                        this.discovering = Some(discovering);
-                        cx.notify()
-                    });
-                }
-                Err(e) => {
-                    let _ = this.update(cx, |this, cx| {
-                        this.error_message = Some(format!("{} error: {e}", line!()));
-                        cx.notify()
-                    });
-                }
-            }
-            let mut events = match adapter.events().await {
-                Ok(x) => x,
-                Err(e) => {
-                    let _ = this.update(cx, |this, cx| {
-                        this.error_message = Some(format!(
-                            "error while getting events of default adapter: {e}"
-                        ));
-                        cx.notify()
-                    });
-                    return;
-                }
-            };
-            while let Some(event) = events.next().await {
-                println!("Bluetooth event = {event:#?}");
-                match event {
-                    AdapterEvent::DeviceAdded(address) => match adapter.device(address) {
-                        // TODO: listen on device event
-                        Ok(device) => {
-                            match device.is_connected().await {
-                                Ok(is_connected) => {
-                                    let _ = this.update(cx, |this, cx| {
-                                        if is_connected {
-                                            this.connected_count += 1;
-                                        }
-                                        this.devices_connected.insert(address, is_connected);
-                                        cx.notify();
-                                    });
-                                }
-                                Err(e) => {
-                                    let _ = this.update(cx, |this, cx| {
-                                        this.error_message =
-                                            Some(format!("Error {}: {e}", line!()));
-                                        cx.notify()
-                                    });
-                                }
-                            }
-                            match device.events().await {
-                                Ok(mut events) => {
-                                    let this = this.clone();
-                                    cx.spawn(async move |cx| {
-                                        while let Some(event) = events.next().await {
-                                            match event {
-                                                DeviceEvent::PropertyChanged(
-                                                    DeviceProperty::Connected(connected),
-                                                ) => {
-                                                    let _ = this.update(cx, |this, cx| {
-                                                        let was_connected = this
-                                                            .devices_connected
-                                                            .insert(address, connected);
-                                                        if let Some(was_connected) = was_connected
-                                                            && was_connected != connected
-                                                        {
-                                                            if connected {
-                                                                this.connected_count += 1;
-                                                            } else {
-                                                                this.connected_count -= 1;
-                                                            }
-                                                        }
-                                                        cx.notify()
-                                                    });
-                                                }
-                                                _ => (),
-                                            }
-                                        }
-                                    })
-                                    .detach();
-                                }
-                                Err(e) => {
-                                    let _ = this.update(cx, |this, cx| {
-                                        this.error_message =
-                                            Some(format!("{} error: {e}", line!()));
-                                        cx.notify()
-                                    });
-                                }
-                            }
-                        }
-                        Err(e) => {
-                            let _ = this.update(cx, |this, cx| {
-                                this.error_message = Some(format!("Error {}: {e}", line!()));
-                                cx.notify()
-                            });
-                        }
-                    },
-                    AdapterEvent::DeviceRemoved(address) => {
-                        let _ = this.update(cx, |this, cx| {
-                            let was_connected = this.devices_connected.remove(&address);
-                            if was_connected == Some(true) {
-                                this.connected_count -= 1;
-                            }
-                            println!("remove device with address `{address}`: {was_connected:?}");
-                            cx.notify()
-                        });
-                    }
-                    AdapterEvent::PropertyChanged(AdapterProperty::Powered(powered)) => {
-                        let _ = this.update(cx, |this, cx| {
-                            this.powered = Some(powered);
-                            cx.notify()
-                        });
-                    }
-                    AdapterEvent::PropertyChanged(AdapterProperty::Discovering(discovering)) => {
-                        let _ = this.update(cx, |this, cx| {
-                            this.discovering = Some(discovering);
-                            cx.notify()
-                        });
-                    }
-                    _ => (),
-                }
-            }
-        }
+    let adapter = match default_adapter().await {
+        Ok(x) => x,
         Err(e) => {
+            tracing::error!(error = %e, "Failed to get default bluetooth adapter");
             let _ = this.update(cx, |this, cx| {
-                this.error_message = Some(format!("error while getting the default adapter: {e}"));
+                this.error_message = Some(format!("Failed to get default bluetooth adapter: {e}"));
+                cx.notify()
+            });
+            return;
+        }
+    };
+    tracing::info!(default_adapter_name = adapter.name());
+    match adapter.is_powered().await {
+        Ok(is_powered) => {
+            tracing::info!(is_powered, "Adapter property");
+            let _ = this.update(cx, |this, cx| {
+                this.powered = Some(is_powered);
                 cx.notify()
             });
         }
+        Err(e) => {
+            tracing::error!(error = %e, "Failed to get if default adapter is powered");
+        }
     }
+    match adapter.is_discovering().await {
+        Ok(discovering) => {
+            tracing::info!(discovering, "Adapter property");
+            let _ = this.update(cx, |this, cx| {
+                this.discovering = Some(discovering);
+                cx.notify()
+            });
+        }
+        Err(e) => {
+            tracing::error!(error = %e, "Failed to get if default adapter is discovering");
+        }
+    }
+    match adapter.device_addresses().await {
+        Ok(addresses) => {
+            for address in addresses {
+                try_monitor_device(&adapter, address, this.clone(), cx).await;
+            }
+        }
+        Err(e) => {
+            tracing::error!(error = %e, "Failed to get addresses of discovered devices");
+        }
+    }
+    let mut events = match adapter.events().await {
+        Ok(x) => x,
+        Err(e) => {
+            tracing::error!(error = %e, "Failed to get event stream of default adapter");
+            let _ = this.update(cx, |this, cx| {
+                this.error_message = Some(format!(
+                    "Failed to get event stream of default adapter: {e}"
+                ));
+                cx.notify()
+            });
+            return;
+        }
+    };
+    while let Some(event) = events.next().await {
+        tracing::debug!(?event, "Bluetooth event");
+        match event {
+            AdapterEvent::DeviceAdded(address) => {
+                try_monitor_device(&adapter, address, this.clone(), cx).await;
+            }
+            AdapterEvent::DeviceRemoved(address) => {
+                let _ = this.update(cx, |this, cx| {
+                    let was_connected = this.connected_devices.remove(&address);
+                    tracing::info!(%address, was_connected, "Removed a device");
+                    cx.notify()
+                });
+            }
+            AdapterEvent::PropertyChanged(AdapterProperty::Powered(powered)) => {
+                tracing::info!(powered, "Adapter property changed");
+                let _ = this.update(cx, |this, cx| {
+                    this.powered = Some(powered);
+                    cx.notify()
+                });
+            }
+            AdapterEvent::PropertyChanged(AdapterProperty::Discovering(discovering)) => {
+                tracing::info!(discovering, "Adapter property changed");
+                let _ = this.update(cx, |this, cx| {
+                    this.discovering = Some(discovering);
+                    cx.notify()
+                });
+            }
+            _ => (),
+        }
+    }
+    tracing::warn!("event stream of default adapter ended");
+}
+
+async fn try_monitor_device(
+    adapter: &Adapter,
+    address: Address,
+    entity: WeakEntity<Bluetooth>,
+    cx: &mut AsyncApp,
+) {
+    let device = match adapter.device(address) {
+        Ok(x) => x,
+        Err(e) => {
+            tracing::error!(%address, error = %e, "Device added, but failed to get the device at that address");
+            return;
+        }
+    };
+    match device.is_connected().await {
+        Ok(is_connected) => {
+            tracing::info!(%address, name = ?device.name().await, is_connected, "Device property");
+            let _ = entity.update(cx, |this, cx| {
+                if is_connected {
+                    this.connected_devices.insert(address);
+                }
+                cx.notify();
+            });
+        }
+        Err(e) => {
+            tracing::error!(%address, name = ?device.name().await, error = %e, "Failed to get if device is connected");
+        }
+    }
+    let mut events = match device.events().await {
+        Ok(x) => x,
+        Err(e) => {
+            tracing::error!(%address, name = ?device.name().await, error = %e, "Failed to get device event stream");
+            return;
+        }
+    };
+    tracing::info!(%address, name = ?device.name().await, "Monitoring a device");
+    cx.spawn(async move |cx| {
+        while let Some(event) = events.next().await {
+            match event {
+                DeviceEvent::PropertyChanged(
+                    DeviceProperty::Connected(connected),
+                ) => {
+                    let _ = entity.update(cx, |this, cx| {
+                        let was_connected = if connected {
+                            this.connected_devices.insert(address)
+                        } else {
+                            this.connected_devices.remove(&address)
+                        };
+                        tracing::info!(%address, connected, was_connected, "Device property changed");
+                        cx.notify()
+                    });
+                }
+                _ => (),
+            }
+        }
+    })
+    .detach();
 }
 
 async fn default_adapter() -> bluer::Result<Adapter> {
