a19bbd1838b079f2854d9614d21ed239c538b0ce
Shiphan <140245703+Shiphan@users.noreply.github.com>
2026-06-03 01:12:32 +0800

    Package in nix

--- flake.nix
+++ flake.nix
@@ -35,5 +35,54 @@
         LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
       };
       formatter.${system} = pkgs.nixfmt-tree;
+
+      packages.${system}.eucalyptus-twig = pkgs.callPackage (
+        {
+          lib,
+          rustPlatform,
+          pkg-config,
+          dbus,
+          fontconfig,
+          freetype,
+          libxkbcommon,
+          pipewire,
+          vulkan-loader,
+          wayland,
+        }:
+
+        rustPlatform.buildRustPackage (finalAttrs: {
+          pname = "eucalyptus-twig";
+          version = "git-${self.shortRev or "dirty"}";
+
+          src = ./.;
+
+          cargoHash = "sha256-cPZUEHXjWllqLSxReufjw2PGy1vPPzPMMWQEOocqAR4=";
+
+          nativeBuildInputs = [
+            pkg-config
+            rustPlatform.bindgenHook
+          ];
+
+          buildInputs = [
+            dbus
+            fontconfig
+            freetype
+            libxkbcommon
+            pipewire
+            vulkan-loader
+            wayland
+          ];
+
+          RUSTFLAGS = "-C link-arg=-Wl,--push-state,--no-as-needed,-lwayland-client,-lvulkan,--pop-state";
+
+          meta = {
+            description = "";
+            homepage = "https://github.com/Shiphan/eucalyptus-twig";
+            license = lib.licenses.gpl2Plus;
+            maintainers = with lib.maintainers; [ shiphan ];
+            mainProgram = "eucalyptus-twig";
+          };
+        })
+      ) { };
     };
 }
